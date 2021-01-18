cc22eeda0186322fb1c5aa816b1bc2ac0cd869ba
JohnWes7 <1246215651@qq.com>
2021-01-18 23:01:09 +0800

    俄罗斯方块半成品

---
+++ Assets/scripts/CubeControlller.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControlller : MonoBehaviour
+{
+    float moveTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //自己下落
+        moveTimer += Time.deltaTime;
+        if (moveTimer > 0.8f)
+        {
+            Move();
+            moveTimer = 0;
+        }
+
+        //按键检测
+        MonitorKey();
+    }
+
+    /// <summary>
+    /// 自己移动
+    /// </summary>
+    void Move()
+    {
+        transform.position += new Vector3(0, -1, 0);
+        if(!IsIn())
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+    }
+
+    /// <summary>
+    /// 按键检测
+    /// </summary>
+    void MonitorKey()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //左移
+            transform.position += new Vector3(-1, 0, 0);
+
+            //是否出界
+            if (!IsIn())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //右移
+            transform.position += new Vector3(1, 0, 0);
+            //是否出界
+            if (!IsIn())
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //旋转
+        }
+    }
+
+    /// <summary>
+    /// 出界判断
+    /// </summary>
+    /// <returns>true表示没出界 false表示出界</returns>
+    bool IsIn()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform temp = transform.GetChild(i);
+            if (temp.position.x < 1 || temp.position.x > 10 || temp.position.y < 1)
+            {
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
+}
---
+++ Assets/scripts/RCManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCManager : MonoBehaviour
+{
+    public float lifeTime;
+    public float lifeTimer;
+
+    public List<GameObject> cubePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubePrefabs = new List<GameObject>();
+
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/L1"));//0
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/L2"));
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/line"));
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/Square"));
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/T"));
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/Z1"));
+        cubePrefabs.Add(Resources.Load<GameObject>("Prefabs/Z2"));
+
+        CreatCube();
+    }
+
+    public void CreatCube()
+    {
+        int index = Random.Range(0, cubePrefabs.Count);
+
+        if (index == 2)
+        {
+            Instantiate<GameObject>(cubePrefabs[index], new Vector3(5.5f, 21, 0), Quaternion.identity);
+        }
+        else if (index == 3)
+        {
+            Instantiate<GameObject>(cubePrefabs[index], new Vector3(5.5f, 21.5f, 0), Quaternion.identity);
+        }
+        else if (index == 4 || index == 5 || index == 6)
+        {
+            Instantiate<GameObject>(cubePrefabs[index], new Vector3(5, 21.5f, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate<GameObject>(cubePrefabs[index], new Vector3(5, 21f, 0), Quaternion.identity);
+        }
+    }
+
+
+}
